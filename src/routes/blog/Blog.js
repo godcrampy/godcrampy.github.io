2344af6ba746873386897f42ae916eadcc891950
Sahil Bondre <sahilbondre@gmail.com>
2022-12-27 15:43:16 +0000

    minor: fix formatting

--- src/routes/blog/Blog.js
+++ src/routes/blog/Blog.js
@@ -1,15 +1,14 @@
-import {useParams} from "react-router-dom";
-import index from "src/blog/index.json"
-import {marked} from "marked";
-import {useEffect, useState} from "react";
+import { useParams } from "react-router-dom";
+import index from "src/blog/index.json";
+import { marked } from "marked";
+import { useEffect, useState } from "react";
 import Prism from "prismjs";
 
 const getMarkdown = async (markdownFilePath) => {
-  return await require(`../../${markdownFilePath}`)
-}
+  return await require(`../../${markdownFilePath}`);
+};
 
 function Blog() {
-
   useEffect(() => {
     Prism.highlightAll();
   });
@@ -17,27 +16,32 @@ function Blog() {
   const blogId = params.blogId;
   const [data, updateData] = useState("");
 
-  Promise.all(index.map(async e => {
-    const location = e.location;
-    const mod = await getMarkdown(location);
-    const file = await fetch(mod);
-    const text = await file.text();
-    const lexer = new marked.Lexer();
-    const tokens = lexer.lex(text);
-    console.log(tokens.map(e => {
-      return {
-        ...e,
-        html: marked.parse(e.raw, {langPrefix: 'language-'})
-      }
-    }))
-    return marked.parse(text, {langPrefix: 'language-'})
-  })).then(res => updateData(res[0]));
+  Promise.all(
+    index.map(async (e) => {
+      const location = e.location;
+      const mod = await getMarkdown(location);
+      const file = await fetch(mod);
+      const text = await file.text();
+      const lexer = new marked.Lexer();
+      const tokens = lexer.lex(text);
+      console.log(
+        tokens.map((e) => {
+          return {
+            ...e,
+            html: marked.parse(e.raw, { langPrefix: "language-" }),
+          };
+        })
+      );
+      return marked.parse(text, { langPrefix: "language-" });
+    })
+  ).then((res) => updateData(res[0]));
 
   return (
-      <div>
-        Welcome to Blog {blogId}!
-        <div dangerouslySetInnerHTML={{__html: data}}></div>
-      </div>);
+    <div>
+      Welcome to Blog {blogId}!
+      <div dangerouslySetInnerHTML={{ __html: data }}></div>
+    </div>
+  );
 }
 
 export default Blog;
